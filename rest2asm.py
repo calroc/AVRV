d1d88599df7b15adeaa655c656ae55b421859483
Simon Forman <forman.simon@gmail.com>
2012-01-28 20:32:29 -0800

    Finished moving code to ReST file
    Can generate HTML and ASM files from ReST file.

--- rest2asm.py
+++ rest2asm.py
@@ -4,7 +4,7 @@ Good enough for now. Read a ReST file and extract all the code definitions
 and print them out. Use this to transform avrvm.rest to avrvm.asm.
 '''
 import sys
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulStoneSoup
 from docutils.core import publish_parts
 
 if len(sys.argv) > 1:
@@ -13,7 +13,10 @@ else:
     data = sys.stdin.read()
 
 parts = publish_parts(data, writer_name='html')
-soup = BeautifulSoup(parts['html_body'])
+soup = BeautifulStoneSoup(
+    parts['html_body'],
+    convertEntities=BeautifulStoneSoup.ALL_ENTITIES,
+    )
 
 for p in soup.findAll('pre', { "class" : "literal-block" }):
     for c in p.contents:
