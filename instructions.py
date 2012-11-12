3a5df27e84c4565e6c2b43532e8a49f95ccbdc07
Simon Forman <forman.simon@gmail.com>
2012-11-12 00:51:28 -0800

    Move InstructionsMixin to instructions.py
    Now all instruction-specific code is in that module.

--- instructions.py
+++ instructions.py
@@ -1,4 +1,4 @@
-from util import A, B, B_reversed, C, D, E, F, G, H
+from util import A, B, B_reversed, C, D, E, F, G, H, instr, spec, spec_reversed
 
 
 _mark = set(dir()) ; _mark.add('_mark')
@@ -339,6 +339,204 @@ ops = dict(
   )
 
 
+class InstructionsMixin(object):
+
+  @instr
+  def jmp(self, address):
+    self.here += 2
+    return address
+
+  @instr
+  def rjmp(self, address):
+    return address
+
+  @instr
+  def rcall(self, address):
+    return address
+
+  @instr
+  def cli(self):
+    pass
+
+  @instr
+  def sei(self):
+    pass
+
+  @instr
+  def ret(self):
+    pass
+
+  @instr
+  def ldi(self, target, address):
+    if isinstance(address, str):
+      assert len(address) == 1, repr(address)
+      address = ord(address)
+    return target, address << 1
+
+  @instr
+  def out(self, target, address):
+    return target, address
+
+  @instr
+  def sts(self, address, register):
+    self.here += 2
+    return address << 1, register
+
+  @instr
+  def mov(self, target, source):
+    return target, source
+
+  @instr
+  def lds(self, target, address):
+    self.here += 2
+    return target, address << 1
+
+  @instr
+  def sbrs(self, target, address):
+    return target, address
+
+  @spec
+  def st_post_incr(self, ptr, register):
+    pass
+
+  @spec_reversed
+  def ld_post_incr(self, register, ptr):
+    pass
+
+  @spec_reversed
+  def ld_pre_decr(self, register, ptr):
+    pass
+
+  @spec_reversed
+  def lpm_post_incr(self, register, ptr):
+    pass
+
+  @instr
+  def cpi(self, register, immediate):
+    if isinstance(immediate, str):
+      assert len(immediate) == 1, repr(immediate)
+      immediate = ord(immediate)
+    return register, immediate << 1
+
+  @instr
+  def brne(self, address):
+    return address
+
+  @instr
+  def breq(self, address):
+    return address
+
+  @instr
+  def inc(self, address):
+    return address
+
+  @instr
+  def mul(self, target, source):
+    return target, source
+
+  @instr
+  def brlo(self, address):
+    return address
+
+  @instr
+  def subi(self, target, source):
+    return target, source
+
+  @instr
+  def add(self, target, source):
+    return target, source
+
+  @instr
+  def dec(self, address):
+    return address
+
+  @instr
+  def clr(self, address):
+    return address
+
+  @instr
+  def lsl(self, address):
+    return address
+
+  @instr
+  def brcc(self, address):
+    return address
+
+  @instr
+  def or_(self, target, source):
+    return target, source
+
+  @instr
+  def push(self, address):
+    return address
+
+  @instr
+  def swap(self, address):
+    return address
+
+  @instr
+  def pop(self, address):
+    return address
+
+  @instr
+  def movw(self, target, source):
+    return target, source
+
+  @instr
+  def andi(self, target, source):
+    return target, source
+
+  @instr
+  def adiw(self, target, source):
+    return target, source
+
+  def lpm(self, target, source):
+    assert source == 30, repr(source) # Must be Z
+    self._one('lpm', target)
+
+  @instr
+  def cp(self, target, source):
+    return target, source
+
+  @instr
+  def cpc(self, target, source):
+    return target, source
+
+  @instr
+  def brsh(self, address):
+    return address
+
+  @instr
+  def cpse(self, target, source):
+    return target, source
+
+  @instr
+  def sbiw(self, target, source):
+    return target, source
+
+  @instr
+  def lsr(self, address):
+    return address
+
+  @instr
+  def ijmp(self):
+    pass
+
+  def _one(self, op, address):
+    name, address = self._name_or_addr(address)
+    addr = self._get_here()
+    print 'assembling %s instruction at %s to %s' % (op, addr, name)
+    self.data[addr] = (op, address)
+    self.here += 2
+
+  def _instruction_namespace(self):
+    for n in dir(InstructionsMixin):
+      if n.startswith('_'):
+        continue
+      yield n, getattr(self, n)
+
+
 if __name__ == '__main__':
   import pprint
   pprint.pprint(ops)
+  pprint.pprint(dict(InstructionsMixin()._instruction_namespace()))
--- pyavrasm.py
+++ pyavrasm.py
@@ -1,8 +1,8 @@
-import sys, pprint
+import pprint
 from collections import defaultdict
 from struct import pack
-from myhdl import intbv, bin as biny
-from pass2 import ops
+from myhdl import intbv
+from instructions import ops, InstructionsMixin
 from m328P_def import defs as G
 from util import (
   update,
@@ -12,207 +12,7 @@ from util import (
   high,
   compute_dw,
   compute_db,
-  instr,
-  spec,
-  spec_reversed,
   )
-
-
-class InstructionsMixin(object):
-
-  @instr
-  def jmp(self, address):
-    self.here += 2
-    return address
-
-  @instr
-  def rjmp(self, address):
-    return address
-
-  @instr
-  def rcall(self, address):
-    return address
-
-  @instr
-  def cli(self):
-    pass
-
-  @instr
-  def sei(self):
-    pass
-
-  @instr
-  def ret(self):
-    pass
-
-  @instr
-  def ldi(self, target, address):
-    if isinstance(address, str):
-      assert len(address) == 1, repr(address)
-      address = ord(address)
-    return target, address << 1
-
-  @instr
-  def out(self, target, address):
-    return target, address
-
-  @instr
-  def sts(self, address, register):
-    self.here += 2
-    return address << 1, register
-
-  @instr
-  def mov(self, target, source):
-    return target, source
-
-  @instr
-  def lds(self, target, address):
-    self.here += 2
-    return target, address << 1
-
-  @instr
-  def sbrs(self, target, address):
-    return target, address
-
-  @spec
-  def st_post_incr(self, ptr, register):
-    pass
-
-  @spec_reversed
-  def ld_post_incr(self, register, ptr):
-    pass
-
-  @spec_reversed
-  def ld_pre_decr(self, register, ptr):
-    pass
-
-  @spec_reversed
-  def lpm_post_incr(self, register, ptr):
-    pass
-
-  @instr
-  def cpi(self, register, immediate):
-    if isinstance(immediate, str):
-      assert len(immediate) == 1, repr(immediate)
-      immediate = ord(immediate)
-    return register, immediate << 1
-
-  @instr
-  def brne(self, address):
-    return address
-
-  @instr
-  def breq(self, address):
-    return address
-
-  @instr
-  def inc(self, address):
-    return address
-
-  @instr
-  def mul(self, target, source):
-    return target, source
-
-  @instr
-  def brlo(self, address):
-    return address
-
-  @instr
-  def subi(self, target, source):
-    return target, source
-
-  @instr
-  def add(self, target, source):
-    return target, source
-
-  @instr
-  def dec(self, address):
-    return address
-
-  @instr
-  def clr(self, address):
-    return address
-
-  @instr
-  def lsl(self, address):
-    return address
-
-  @instr
-  def brcc(self, address):
-    return address
-
-  @instr
-  def or_(self, target, source):
-    return target, source
-
-  @instr
-  def push(self, address):
-    return address
-
-  @instr
-  def swap(self, address):
-    return address
-
-  @instr
-  def pop(self, address):
-    return address
-
-  @instr
-  def movw(self, target, source):
-    return target, source
-
-  @instr
-  def andi(self, target, source):
-    return target, source
-
-  @instr
-  def adiw(self, target, source):
-    return target, source
-
-  def lpm(self, target, source):
-    assert source == 30, repr(source) # Must be Z
-    self._one('lpm', target)
-
-  @instr
-  def cp(self, target, source):
-    return target, source
-
-  @instr
-  def cpc(self, target, source):
-    return target, source
-
-  @instr
-  def brsh(self, address):
-    return address
-
-  @instr
-  def cpse(self, target, source):
-    return target, source
-
-  @instr
-  def sbiw(self, target, source):
-    return target, source
-
-  @instr
-  def lsr(self, address):
-    return address
-
-  @instr
-  def ijmp(self):
-    pass
-
-  def _one(self, op, address):
-    name, address = self._name_or_addr(address)
-    addr = self._get_here()
-    print 'assembling %s instruction at %s to %s' % (op, addr, name)
-    self.data[addr] = (op, address)
-    self.here += 2
-
-  def _instruction_namespace(self):
-    for n in dir(InstructionsMixin):
-      if n.startswith('_'):
-        continue
-      yield n, getattr(self, n)
 
 
 class AVRAssembly(InstructionsMixin, object):
@@ -270,8 +70,6 @@ class AVRAssembly(InstructionsMixin, object):
     print 'assembling %i data words at %s for %s => %r' % (nbytes/2, addr, values, data)
     self.data[addr] = ('dw', values, data)
     self.here += nbytes
-##    values_bin_str = values_to_dw(values)
-##    self.here += len(values_bin_str)
 
   def db(self, *values):
     addr = self._get_here()
@@ -374,4 +172,5 @@ if __name__ == '__main__':
       ih.puts(addr, val)
     else:
       print 'non-str', addr, repr(val)
-  ih.dump(tofile=open('pavr.hex', 'w'))
+##  ih.dump(tofile=open('pavr.hex', 'w'))
+  ih.dump()
